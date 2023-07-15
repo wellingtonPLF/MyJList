057ff067e6d0469ec84be0224b12272fce293662
Wellignton Pessoa de Lima Filho <wellplf@gmail.com>
2023-07-15 14:30:52 -0300

    Improving HomePage 02

--- 3° FrontEnd/src/app/views/HomeScreen/HomeScript.ts
+++ 3° FrontEnd/src/app/views/HomeScreen/HomeScript.ts
@@ -1,8 +1,34 @@
-import HeaderComponent from "./../../components/_main/Header/HeaderComponent.vue"
+import HeaderComponent from "./../../components/_main/Header/HeaderComponent.vue";
+import axios, { AxiosResponse } from "axios";
 
-export default{
-    name: 'MyComponent',
-    components: {
-        HeaderComponent
-    }
+interface Game {
+  id: number;
+  img: string;
+  name: string;
+  plataforms: string[];
 }
+
+const homeComponent: any = {
+  name: "SignInPage",
+  components: {
+    HeaderComponent,
+  },
+  data() {
+    return {
+      games: [] as Game[],
+    };
+  },
+  mounted() {
+    axios
+      .get("jsons/games.json")
+      .then((it: AxiosResponse<Game[]>) => {
+        this.games = it.data;
+        console.log(this.games[0].plataforms)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
+
+export default homeComponent;
