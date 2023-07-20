22f84a9bfef476f50bacf9cf3c97184c40e6253f
Wellignton Pessoa de Lima Filho <wellplf@gmail.com>
2023-07-19 22:39:18 -0300

    Add user Page

--- 3° FrontEnd/src/app/views/UserScreen/UserScript.ts
+++ 3° FrontEnd/src/app/views/UserScreen/UserScript.ts
@@ -11,14 +11,18 @@ const userComponent: any = {
   components: {},
   data() {
     return {
-      users: [] as User[]
+      users: [] as User[],
+      imgType: {
+        male: "https://storage.prompt-hunt.workers.dev/clgrgds4b000qmh08559h5fk1_1",
+        female: "https://img.freepik.com/premium-photo/cute-girl-3d-character-design-cartoon-girl-avatar_432516-5510.jpg?w=2000"
+      }
     };
   },
   mounted() {
     axios
       .get("jsons/users.json")
       .then((it: AxiosResponse<User[]>) => {
-        this.users = it.data;
+        this.users = it.data.slice(0, 20);
       })
       .catch((error) => {
         console.error(error);
