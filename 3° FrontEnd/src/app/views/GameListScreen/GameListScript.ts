6c3c0b8b7b4bee38e9e971cdc36063a8d678427c
Wellignton Pessoa de Lima Filho <wellplf@gmail.com>
2023-08-02 20:37:24 -0300

    Improving GameList Page 03

--- 3° FrontEnd/src/app/views/GameListScreen/GameListScript.ts
+++ 3° FrontEnd/src/app/views/GameListScreen/GameListScript.ts
@@ -1,10 +1,34 @@
 
+import axios, { AxiosResponse } from "axios";
+
+interface Game {
+  id: number;
+  img: string;
+  name: string;
+  plataforms: string[];
+  user: string;
+  date: string;
+  score: string;
+  description: string;
+}
+
 const gameListComponent: any = {
   name: "GameListComponent",
   data() {
-    return {};
+    return {
+      games: [] as Game[]
+    };
   },
-  mounted() { },
+  mounted() {
+    axios
+      .get("jsons/games.json")
+      .then((it: AxiosResponse<Game[]>) => {
+        this.games = it.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
 };
 
 export default gameListComponent;
