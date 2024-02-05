0b8c41810837b66effce69d1d71c371fe8509967
Wellignton Pessoa de Lima Filho <wellplf@gmail.com>
2024-02-05 09:11:23 -0300

    Fixing Images 02

--- 3° FrontEnd/src/app/shared/enums/sexualityImg.ts
+++ 3° FrontEnd/src/app/shared/enums/sexualityImg.ts
@@ -1,4 +1,4 @@
 export const sexualityImg = {
-    "male": `${./assets/images/monster.png`,
-    "female": `${./assets/images/pink.png`,
+    "male": `./assets/images/monster.png`,
+    "female": `./assets/images/pink.png`,
 }
